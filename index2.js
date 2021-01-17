2679bc05e38191c877a98f3c1412d0f550864e52
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-17 06:46:48 +0000

    Final ISS

--- index2.js
+++ index2.js
@@ -1,9 +1,20 @@
-const { fetchMyIP } = require('./iss_promised');
-const { fetchCoordsByIP } = require('./iss_promised');
-const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+const printPassTimes = function(passTimes) {
+
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
   
